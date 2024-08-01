d3549d05ed3584e2582d2496e2efefeac7ca0119
Tomek Marciniak <tmarciniakm@gmail.com>
2024-08-02 00:01:07 +0200

    feat: add exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/accounts/index.ts", "src/providers/index.ts"],
 	outDir: "./dist",
-	format: "esm",
+	format: ["esm", "cjs"],
 	sourcemap: true,
 	clean: true,
 	bundle: true,
