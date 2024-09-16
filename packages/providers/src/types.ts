465e330ec50a72cf51e5141c5a66421c4d471792
Tomek Marciniak <tmarciniakm@gmail.com>
2024-09-16 08:32:24 +0200

    feat(providers): rewrite validators

--- packages/providers/src/types.ts
+++ packages/providers/src/types.ts
@@ -1,29 +1,16 @@
+import type { z } from "zod";
 import type {
-	CreateNullifierParams,
-	Nullifier,
-	PublicKey,
-	SendTransactionParams,
-	SignFieldsParams,
-	SignMessageParams,
-	SignTransactionParams,
-	SignedFields,
-	SignedMessage,
-	SignedTransaction,
-	TransactionReceipt,
-} from "@mina-js/shared";
-import type { AddChainData, SwitchChainData } from "./validation";
+	MinaProviderClientSchema,
+	MinaProviderDetailSchema,
+	MinaProviderInfoSchema,
+	ProviderListenerSchema,
+	ProviderRequestSchema,
+	ProviderRpcErrorSchema,
+} from "./validation";
 
-export type MinaProviderDetail = {
-	info: MinaProviderInfo;
-	provider: MinaProviderClient;
-};
+export type MinaProviderDetail = z.infer<typeof MinaProviderDetailSchema>;
 
-export type MinaProviderInfo = {
-	icon: `data:image/${string}`;
-	name: string;
-	rdns: string;
-	slug: string;
-};
+export type MinaProviderInfo = z.infer<typeof MinaProviderInfoSchema>;
 
 export interface MinaAnnounceProviderEvent
 	extends CustomEvent<MinaProviderDetail> {
@@ -34,14 +21,7 @@ export interface MinaRequestProviderEvent extends Event {
 	type: "mina:requestProvider";
 }
 
-export type ProviderRpcError = (
-	| { message: "User Rejected Request"; code: 4001 }
-	| { message: "Unauthorized"; code: 4100 }
-	| { message: "Unsupported Method"; code: 4200 }
-	| { message: "Disconnected"; code: 4900 }
-	| { message: "Chain Disconnected"; code: 4901 }
-) &
-	Error;
+export type ProviderRpcError = z.infer<typeof ProviderRpcErrorSchema> & Error;
 
 export type ProviderRpcEvent =
 	| "connect"
@@ -50,105 +30,8 @@ export type ProviderRpcEvent =
 	| "accountsChanged"
 	| "mina_message";
 
-// Request variants
-export type AccountsRequest = (args: {
-	method: "mina_accounts";
-}) => Promise<PublicKey[]>;
+export type ProviderRequest = z.infer<typeof ProviderRequestSchema>;
 
-export type ChainIdRequest = (args: {
-	method: "mina_chainId";
-}) => Promise<string>;
+export type ProviderListener = z.infer<typeof ProviderListenerSchema>;
 
-export type ChainInformationRequest = (args: {
-	method: "mina_chainInformation";
-}) => Promise<{ url: string; name: string }>;
-
-export type GetBalanceRequest = (args: {
-	method: "mina_getBalance";
-}) => Promise<number>;
-
-export type SignRequest = (args: {
-	method: "mina_sign";
-	params: SignMessageParams;
-}) => Promise<SignedMessage>;
-
-export type SignFieldsRequest = (args: {
-	method: "mina_signFields";
-	params: SignFieldsParams;
-}) => Promise<SignedFields>;
-
-export type SignTransactionRequest = (args: {
-	method: "mina_signTransaction";
-	params: SignTransactionParams;
-}) => Promise<SignedTransaction>;
-
-export type SendTransactionRequest = (args: {
-	method: "mina_sendTransaction";
-	params: SendTransactionParams;
-}) => Promise<TransactionReceipt>;
-
-export type CreateNullifierRequest = (args: {
-	method: "mina_createNullifier";
-	params: CreateNullifierParams;
-}) => Promise<Nullifier>;
-
-export type SwitchChainRequest = (args: {
-	method: "mina_switchChain";
-	params: SwitchChainData;
-}) => Promise<string>;
-
-export type AddChainRequest = (args: {
-	method: "mina_addChain";
-	params: AddChainData;
-}) => Promise<string>;
-
-export type ProviderRequest =
-	| AccountsRequest
-	| ChainIdRequest
-	| ChainInformationRequest
-	| GetBalanceRequest
-	| SignRequest
-	| SignFieldsRequest
-	| SignTransactionRequest
-	| SendTransactionRequest
-	| CreateNullifierRequest
-	| SwitchChainRequest
-	| AddChainRequest;
-
-export type ConnectedListener = (
-	event: "connected",
-	callback: (params: { chainId: string }) => void,
-) => void;
-
-export type DisconnectedListener = (
-	event: "disconnected",
-	callback: (params: { error: ProviderRpcError }) => void,
-) => void;
-
-export type ChainChangedListener = (
-	event: "chainChanged",
-	callback: (params: { chainId: string }) => void,
-) => void;
-
-export type AccountsChangedListener = (
-	event: "accountsChanged",
-	callback: (params: { accounts: PublicKey[] }) => void,
-) => void;
-
-export type MessageListener = (
-	event: "mina_message",
-	callback: (params: { type: string; data: unknown }) => void,
-) => void;
-
-export type ProviderListener =
-	| ConnectedListener
-	| DisconnectedListener
-	| ChainChangedListener
-	| AccountsChangedListener
-	| MessageListener;
-
-export type MinaProviderClient = {
-	request: ProviderRequest;
-	on: ProviderListener;
-	removeListener: ProviderListener;
-};
+export type MinaProviderClient = z.infer<typeof MinaProviderClientSchema>;
--- packages/providers/src/validation.ts
+++ packages/providers/src/validation.ts
@@ -1,14 +1,192 @@
+import {
+	CreateNullifierParamsSchema,
+	NullifierSchema,
+	PublicKeySchema,
+	SendTransactionParamsSchema,
+	SignFieldsParamsSchema,
+	SignMessageParamsSchema,
+	SignTransactionParamsSchema,
+	SignedFieldsSchema,
+	SignedMessageSchema,
+	SignedTransactionSchema,
+	TransactionReceiptSchema,
+} from "@mina-js/shared";
 import { z } from "zod";
 
-export const switchChainRequestSchema = z.object({
+export const SwitchChainRequestParams = z.object({
 	chainId: z.string(),
 });
 
-export type SwitchChainData = z.infer<typeof switchChainRequestSchema>;
-
-export const addChainRequestSchema = z.object({
+export const AddChainRequestParams = z.object({
 	url: z.string().url(),
 	name: z.string(),
 });
 
-export type AddChainData = z.infer<typeof addChainRequestSchema>;
+export const AccountsRequestSchema = z
+	.function()
+	.args(z.object({ method: z.literal("mina_accounts") }))
+	.returns(z.promise(z.array(PublicKeySchema)));
+export const ChainIdRequestSchema = z
+	.function()
+	.args(z.object({ method: z.literal("mina_chainId") }))
+	.returns(z.promise(z.string()));
+export const ChainInformationRequestSchema = z
+	.function()
+	.args(z.object({ method: z.literal("mina_chainInformation") }))
+	.returns(z.promise(z.object({ url: z.string(), name: z.string() })));
+export const GetBalanceRequestSchema = z
+	.function()
+	.args(z.object({ method: z.literal("mina_getBalance") }))
+	.returns(z.promise(z.number()));
+export const SignRequestSchema = z
+	.function()
+	.args(
+		z.object({
+			method: z.literal("mina_sign"),
+			params: SignMessageParamsSchema,
+		}),
+	)
+	.returns(z.promise(SignedMessageSchema));
+export const SignFieldsRequestSchema = z
+	.function()
+	.args(
+		z.object({
+			method: z.literal("mina_signFields"),
+			params: SignFieldsParamsSchema,
+		}),
+	)
+	.returns(z.promise(SignedFieldsSchema));
+export const SignTransactionRequestSchema = z
+	.function()
+	.args(
+		z.object({
+			method: z.literal("mina_signTransaction"),
+			params: SignTransactionParamsSchema,
+		}),
+	)
+	.returns(z.promise(SignedTransactionSchema));
+export const SendTransactionRequestSchema = z
+	.function()
+	.args(
+		z.object({
+			method: z.literal("mina_sendTransaction"),
+			params: SendTransactionParamsSchema,
+		}),
+	)
+	.returns(z.promise(TransactionReceiptSchema));
+export const CreateNullifierRequestSchema = z
+	.function()
+	.args(
+		z.object({
+			method: z.literal("mina_createNullifier"),
+			params: CreateNullifierParamsSchema,
+		}),
+	)
+	.returns(z.promise(NullifierSchema));
+export const SwitchChainRequestSchema = z
+	.function()
+	.args(
+		z.object({
+			method: z.literal("mina_switchChain"),
+			params: SwitchChainRequestParams,
+		}),
+	)
+	.returns(z.promise(z.string()));
+export const AddChainRequestSchema = z
+	.function()
+	.args(
+		z.object({
+			method: z.literal("mina_addChain"),
+			params: AddChainRequestParams,
+		}),
+	)
+	.returns(z.promise(z.string()));
+
+export const ProviderRequestSchema = z.union([
+	AccountsRequestSchema,
+	ChainIdRequestSchema,
+	ChainInformationRequestSchema,
+	GetBalanceRequestSchema,
+	SignRequestSchema,
+	SignFieldsRequestSchema,
+	SignTransactionRequestSchema,
+	SendTransactionRequestSchema,
+	CreateNullifierRequestSchema,
+	SwitchChainRequestSchema,
+	AddChainRequestSchema,
+]);
+
+export const ChainIdCallbackSchema = z
+	.function()
+	.args(z.object({ chainId: z.string() }))
+	.returns(z.void());
+
+export const ConnectedListenerSchema = z
+	.function()
+	.args(z.literal("connected"), ChainIdCallbackSchema)
+	.returns(z.void());
+export const DisconnectedListenerSchema = z
+	.function()
+	.args(z.literal("disconnected"), z.function())
+	.returns(z.void());
+export const ChainChangedListenerSchema = z
+	.function()
+	.args(z.literal("chainChanged"), ChainIdCallbackSchema)
+	.returns(z.void());
+export const AccountsChangedListenerSchema = z
+	.function()
+	.args(z.literal("accountsChanged"), z.function())
+	.returns(z.void());
+export const MessageListenerSchema = z
+	.function()
+	.args(z.literal("mina_message"), z.function())
+	.returns(z.void());
+
+export const ProviderListenerSchema = z.union([
+	ConnectedListenerSchema,
+	DisconnectedListenerSchema,
+	ChainChangedListenerSchema,
+	AccountsChangedListenerSchema,
+	MessageListenerSchema,
+]);
+
+export const ProviderRpcErrorSchema = z.discriminatedUnion("code", [
+	z.object({
+		code: z.literal(4001),
+		message: z.literal("User Rejected Request"),
+	}),
+	z.object({
+		code: z.literal(4100),
+		message: z.literal("Unauthorized"),
+	}),
+	z.object({
+		code: z.literal(4200),
+		message: z.literal("Unsupported Method"),
+	}),
+	z.object({
+		code: z.literal(4900),
+		message: z.literal("Disconnected"),
+	}),
+	z.object({
+		code: z.literal(4901),
+		message: z.literal("Chain Disconnected"),
+	}),
+]);
+
+export const MinaProviderInfoSchema = z.object({
+	icon: z.string().startsWith("data:image/"),
+	name: z.string(),
+	rdns: z.string(),
+	slug: z.string(),
+});
+
+export const MinaProviderClientSchema = z.object({
+	request: ProviderRequestSchema,
+	on: ProviderListenerSchema,
+	removeListener: ProviderListenerSchema,
+});
+
+export const MinaProviderDetailSchema = z.object({
+	info: MinaProviderInfoSchema,
+	provider: MinaProviderClientSchema,
+});
