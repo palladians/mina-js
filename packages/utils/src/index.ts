73d023ee6ebb6a56424d105a0a10d619f8fce3c0
Tomek Marciniak <tmarciniakm@gmail.com>
2024-10-09 14:55:05 +0200

    feat(minajs): add utils exports

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,7 @@
 export * from "./types";
 export * from "./validation";
+export { formatMina } from "./src/format-mina";
+export { formatUnits } from "./src/format-units";
+export { parseMina } from "./src/parse-mina";
+export { parseUnits } from "./src/parse-units";
 export * as Test from "./test/constants";
