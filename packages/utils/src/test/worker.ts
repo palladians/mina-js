4eb079d69c4c882fd5825efe7df870ac8b6c0a99
Tomasz Marciniak <tmarciniakm@gmail.com>
2024-11-29 20:21:02 +0100

    chore(worker rpc): improve error throwing

--- packages/utils/src/test/worker.ts
+++ packages/utils/src/test/worker.ts
@@ -1,9 +1,9 @@
 import { createRpcHandler } from "../worker-rpc";
 
 const { messageHandler } = createRpcHandler({
-  methods: {
-    ping: async () => 'pong',
-  }
-})
+	methods: {
+		ping: async () => "pong",
+	},
+});
 
-self.onmessage = messageHandler
+self.onmessage = messageHandler;
